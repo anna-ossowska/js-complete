91b4ea2cdfef261da66240e039de4c3c76a333d6
Anna Ossowska <ossowska.anna1@gmail.com>
2021-05-18 21:25:35 +0200

    Resetting the game

--- 01-DOM/03-Dice-Game/script.js
+++ 01-DOM/03-Dice-Game/script.js
@@ -15,6 +15,25 @@ const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  diceEl.classList.add('hide');
+};
+
+init();
+
 const switchPlayers = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -22,6 +41,9 @@ const switchPlayers = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+// New Game
+btnNew.addEventListener('click', init);
 
 // Rolling the dice
 btnRoll.addEventListener('click', function () {
