c7d2c1384c156a376ce50efe6ccba7d3e683c616
Anna Ossowska <ossowska.anna1@gmail.com>
2021-05-24 21:28:16 +0200

    Maps

--- 02-Data-Structures-Operators/script.js
+++ 02-Data-Structures-Operators/script.js
@@ -383,3 +383,44 @@ console.log(new Set(staff).size);
 
 // Removing duplicated letters from a string
 console.log(new Set('Anna'.toLowerCase()));
+
+// ---- MAPS ----
+
+const rest = new Map();
+rest.set('restName', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+
+console.log(rest.set(2, 'Lisbon, Portugal')); // set() method returns the updated Map
+
+// it allows us to chain the set() methods:
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+// other useful methods and properties:
+
+console.log(rest.get('restName'));
+
+const time = new Date().getHours();
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+
+rest.delete(2);
+
+// rest.clear();
+
+console.log(rest.size);
+
+// We can use an array as a key
+const arrMap = [1, 2];
+rest.set(arrMap, 'Test');
+
+console.log(rest.get(arrMap));
+
+// Passing the DOM object as a key
+rest.set(document.body, 'Body');
+console.log(rest);
