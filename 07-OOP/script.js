86128c86bf7d7efe269a09bc57830df817affceb
Anna Ossowska <ossowska.anna1@gmail.com>
2021-06-12 15:40:10 +0200

    Prototypes

--- 07-OOP/script.js
+++ 07-OOP/script.js
@@ -27,3 +27,30 @@ console.log(john, jane);
 // console.log(john.calcAge());
 
 console.log(john instanceof Person);
+
+// ------ PROTOTYPES ------
+
+// all the objects that are created throught the Person constructor function,
+// will get access to all the methods and properties that are defined on the prototype property
+
+// setting methods on Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+john.calcAge();
+jane.calcAge();
+
+console.log(john.__proto__);
+console.log(Person.prototype);
+
+console.log(john.__proto__ === Person.prototype); // true
+console.log(Person.prototype.isPrototypeOf(john)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// setting properties on Prototypes
+Person.prototype.species = 'Homo Sapiens';
+console.log(john.species, jane.species);
+
+console.log(john.hasOwnProperty('firstName'));
+console.log(john.hasOwnProperty('species'));
