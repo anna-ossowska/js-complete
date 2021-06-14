57369e00ecc029d199a1b3b766ea07be5b41e94c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-06-14 18:57:41 +0200

    Inheritance: exercises

--- 07-OOP/script.js
+++ 07-OOP/script.js
@@ -256,3 +256,54 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  console.log(`${this.make} going at ${(this.speed += 10)} km/h`);
+};
+
+Car.prototype.brake = function () {
+  console.log(`${this.make} going at ${(this.speed -= 5)} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  return (this.charge = chargeTo);
+};
+
+// POLYMORPHISM
+// accelerate method overwrites the accelerate method inherited from the parent class
+EV.prototype.accelerate = function () {
+  console.log(
+    `${
+      this.make
+    } going at ${(this.speed += 20)}, with a charge of ${(this.charge -= 1)}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate();
+tesla.chargeBattery(90);
+tesla.brake();
+tesla.accelerate();
