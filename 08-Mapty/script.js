9de7e169dd9fbd6244eae8a5326173a67e1c0ef1
Anna Ossowska <ossowska.anna1@gmail.com>
2021-06-18 10:31:03 +0200

    Creating a new workout

--- 08-Mapty/script.js
+++ 08-Mapty/script.js
@@ -3,14 +3,6 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
-
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
@@ -22,6 +14,7 @@ class Workout {
 }
 
 class Running extends Workout {
+  type = 'running';
   constructor(coords, distance, duration, cadence) {
     // initialising the 'this' keyword
     super(coords, distance, duration);
@@ -37,6 +30,7 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = 'cycling';
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
@@ -54,16 +48,23 @@ class Cycling extends Workout {
 // const cycling1 = new Cycling([60, 4], 4, 10, 200);
 // console.log(run1, cycling1);
 
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
 /////////////////////////////
 // APPLICATION ARCHITECTURE
 class App {
   // private instance properties
   #map;
   #mapEvent;
-
+  #workouts = [];
   constructor() {
     this._getPosition();
-
     // Event Listeners
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
@@ -120,9 +121,68 @@ class App {
   _newWorkout(e) {
     e.preventDefault();
 
-    // Display marker
+    // Helper functions
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    // Get data from the form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
     const { lat, lng } = this.#mapEvent.latlng;
-    L.marker([lat, lng], { opacity: 0.8 })
+    let workout;
+
+    // If workout running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      // Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Please provide the positive numeric values');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    // If workout cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      // Check if data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      ) {
+        return alert('Please provide the positive numeric values');
+      }
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+    // Add new object to the workout array
+    this.#workouts.push(workout);
+    console.log(this.#workouts);
+
+    // Render workout as a marker on the map
+    //// we call method ourselves using 'this' keyword
+    //// it's not a callback, thus no bind is needed, and 'this' keyword points to the current object
+    this.renderWorkoutMarker(workout);
+
+    // Render workout on the list
+
+    // Hide the form and clear the input fields
+
+    // Clear input fields
+    // prettier-ignore
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+  }
+
+  renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -131,20 +191,12 @@ class App {
           autoClose: false,
           closeOnClick: false,
           // assigning our own css style
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('Workout')
+      .setPopupContent(`${workout.distance} km`)
       .openPopup();
-
-    // Clear input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
   }
 }
 
 const app = new App();
-// const x = (new Date() + '').slice(-10);
