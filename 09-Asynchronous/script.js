670366d0c34fd596466bf4d55f68f9b9506cf29d
Anna Ossowska <ossowska.anna1@gmail.com>
2021-06-20 18:06:27 +0200

    Chaining Promises

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -160,10 +160,31 @@ const renderCountry = function (data, className = '') {
 //     });
 // };
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
+// };
+
+///////////////////////////////////////
+// Chaining promises
+
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('Norway');
