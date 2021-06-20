ee7e177b5ff5840417d999111686af8bfb79ffcd
Anna Ossowska <ossowska.anna1@gmail.com>
2021-06-20 19:07:17 +0200

    Handling rejected Promises

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -3,6 +3,31 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} mln</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+    `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 // XMLHttpRequest
 
@@ -117,26 +142,6 @@ setTimeout(() => {
 ///////////////////////////////////////
 // Promises and fetch API
 
-const renderCountry = function (data, className = '') {
-  const html = `
-      <article class="country ${className}">
-      <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(
-            +data.population / 1000000
-          ).toFixed(1)} mln</p>
-          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-      </article>
-    `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
 // fetch returns a Promise
 // response argument is a value of the fulfilled promise
 // to read the data comming from the response (from response body), we need to call the json() method on the response object
@@ -168,7 +173,7 @@ const renderCountry = function (data, className = '') {
 
 ///////////////////////////////////////
 // Chaining promises
-
+/*
 const getCountryData = function (country) {
   // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
@@ -188,3 +193,37 @@ const getCountryData = function (country) {
 };
 
 getCountryData('Norway');
+
+*/
+
+///////////////////////////////////////
+// Handling rejected promises
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(err);
+      renderError(`Something went wrong: ${err.message}`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('Norway');
+});
